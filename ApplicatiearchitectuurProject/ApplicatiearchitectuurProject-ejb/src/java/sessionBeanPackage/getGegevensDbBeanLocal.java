6733d4a8af50cc486ddeeb203ebc0b4659d133cd
Ruben_Kindt <rubenkindt@live.be>
2019-11-13 14:16:30 +0100

    setup

---
+++ ApplicatiearchitectuurProject/ApplicatiearchitectuurProject-ejb/src/java/sessionBeanPackage/getGegevensDbBeanLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessionBeanPackage;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author r0656495
+ */
+@Local
+public interface getGegevensDbBeanLocal {
+    
+}
